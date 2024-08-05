3221c2123161fc9025695797b0c99a94aa23d9ee
Precious Sukhi <precious.sukhi@gmail.com>
2024-08-05 18:27:50 +0530

    removed heroku

--- client/src/Components/ShowVideo/ShowVideo.jsx
+++ client/src/Components/ShowVideo/ShowVideo.jsx
@@ -9,7 +9,6 @@ function ShowVideo({vid}) {
      <Link to={`/videopage/${vid?._id}`}>
         <video 
         src={`https://youtubenullclass22.onrender.com${vid.filePath}`}
-        // src={`https://youtubeclone5031.herokuapp.com/${vid.filePath}`}
         className="video_ShowVideo"
         />
      </Link>
